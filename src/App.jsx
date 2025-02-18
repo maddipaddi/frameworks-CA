efbff609a62ad35e3eb1cd132b043885b4240b59
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-02-18 12:41:26 +0100

    feat: add a Header component with a nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
-function App() {}
+import Header from "./components/layout/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
 
 export default App;
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,20 @@
+function Header() {
+  return (
+    <header>
+      <h1>ClicketyCart</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <i className="fa-solid fa-cart-shopping"></i>
+    </header>
+  );
+}
+
+export default Header;
