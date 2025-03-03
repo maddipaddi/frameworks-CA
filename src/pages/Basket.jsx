08d5e6c79f00b39394f9d8d11b53a0dc1df19065
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2025-03-03 12:09:13 +0100

    feat: implemnt cart page and checkout page

---
+++ src/pages/Basket.jsx
@@ -0,0 +1,16 @@
+function Basket() {
+  return (
+    <>
+      <div>
+        <h1>Cart Page</h1>
+        <p>Items diplay: ...</p>
+      </div>
+      <div>
+        <h2>Checkout card</h2>
+        <button className="bg-primary text-white">Checkout btn</button>
+      </div>
+    </>
+  );
+}
+
+export default Basket;
