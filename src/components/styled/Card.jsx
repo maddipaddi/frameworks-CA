39848aae9902017a833ea55d3bfd35d06d2435ae
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-03-06 14:11:23 +0100

    feat: add cart styled component

---
+++ src/components/styled/Card.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  background-color: #12172f;
+  color: #fff;
+  border: solid #007bff 2px;
+  border-radius: 1rem;
+  padding: 2rem;
+  margin: 2rem auto;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  align-items: center;
+`;
+
+export default Card;
