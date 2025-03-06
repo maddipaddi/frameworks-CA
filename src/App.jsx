f37ca06c19a499e5987b99967c9f685a0d50cb0a
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2025-03-06 13:27:32 +0100

    feat: global cart function too add items to basket

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import Home from "./pages/Home";
 import Layout from "./components/layout/Layout";
 import SingleProduct from "./pages/SingleProduct";
 import Basket from "./pages/Basket";
+import ProductCard from "./components/ProductCard";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch("https://v2.api.noroff.dev/online-shop")
@@ -19,17 +21,49 @@ function App() {
       .catch((error) => console.error("Error fetching products:", error));
   }, []);
 
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.id === product.id);
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      }
+      return [...prevCart, { ...product, quantity: 1 }];
+    });
+  };
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Layout products={products} />}>
-          <Route index element={<Home products={products} />} />
+        <Route path="/" element={<Layout products={products} cart={cart} />}>
+          <Route
+            index
+            element={<Home products={products} addToCart={addToCart} />}
+          />
           <Route path="/contact" element={<Contact />} />
           <Route
             path="/product/:id"
-            element={<SingleProduct products={products} />}
+            element={
+              <SingleProduct products={products} addToCart={addToCart} />
+            }
           />
-          <Route path="/Basket" element={<Basket />} />
+          <Route
+            path="/product/:id"
+            element={
+              <ProductCard
+                products={products}
+                onProductClick={null}
+                addToCart={addToCart}
+              />
+            }
+          />
+          <Route
+            path="/basket"
+            element={<Basket cart={cart} setCart={setCart} />}
+          />
         </Route>
       </Routes>
     </div>
