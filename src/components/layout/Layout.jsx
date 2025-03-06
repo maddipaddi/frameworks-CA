0bb94663e75c0c55ebc89938322894c549700438
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2025-03-06 13:31:49 +0100

    feat: update hedar too register cart function

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,10 +3,10 @@ import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 import PropTypes from "prop-types";
 
-function Layout({ products }) {
+function Layout({ products, cart }) {
   return (
     <div>
-      <Header products={products} />
+      <Header products={products} cart={cart} />
       <Outlet />
       <Footer />
     </div>
@@ -15,6 +15,7 @@ function Layout({ products }) {
 
 Layout.propTypes = {
   products: PropTypes.array.isRequired,
+  cart: PropTypes.array.isRequired,
 };
 
 export default Layout;
