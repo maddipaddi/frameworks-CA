3d8813926d1ab510758334243298f2780ccdb0ca
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2025-03-06 13:32:53 +0100

    feat: handle add to cart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,21 @@
 import ProductCard from "../components/ProductCard";
 import PropTypes from "prop-types";
 
-function Home({ products }) {
+function Home({ products, addToCart }) {
+  const handleAddToCart = (product) => {
+    addToCart(product);
+  };
+
   return (
-    <>
-      <div>
-        <ProductCard products={products} />
-      </div>
-    </>
+    <div>
+      <ProductCard products={products} addToCart={handleAddToCart} />
+    </div>
   );
 }
 
 Home.propTypes = {
   products: PropTypes.array.isRequired,
+  addToCart: PropTypes.func.isRequired,
 };
 
 export default Home;
