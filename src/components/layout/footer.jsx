45d38be2e194eae51c241cf5982b280e17f05877
Madelen Sletteberg <madelen.sletteberg@gmail.com>
2025-03-09 13:15:14 +0100

    chore: remove footer file

--- src/components/layout/footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="text-center bg-[#132661] text-white fixed bottom-0 w-full py-2">
-      <p>&copy; {new Date().getFullYear()} ClicketyCart</p>
-    </footer>
-  );
-}
-
-export default Footer;
